09db09f30e64008ac959540cbc254318dc672e6d
onimuraaika <aika.onimura@gmail.com>
2021-03-09 15:58:57 +0900

    [Start/Add]配列の書き方練習

--- practice2.js
+++ practice2.js
@@ -1,3 +1,10 @@
-var num;
-num = 1;
-console.log(num);
+let a = new Array(3); //変数aに要素数3の配列を宣言
+
+//要素に代入
+a[0] = "tanaka";
+a[1] = "sato";
+a[2] = "suzuki";
+
+console.log(a[0]);
+console.log(a[1]);
+console.log(a[2]);
