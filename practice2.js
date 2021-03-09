9f08daf3c823ac99408384a5e66f2fecd974c335
onimuraaika <aika.onimura@gmail.com>
2021-03-09 16:49:22 +0900

    [Add]演算子の練習、インクリメントとデクリメント

--- practice2.js
+++ practice2.js
@@ -1,9 +1,8 @@
-let a = 1;
-let b = 2;
-let c = 3;
+let a = 0;
+let b = 10;
 
-a += 10; //a = a + 10
-c += b; //b = b + c
+a++; //a = a + 1　インクリメント（1増やす）
+b--; //b = b - 1　デクリメント（1減らす）
 
 console.log(a);
-console.log(c);
+console.log(b);
