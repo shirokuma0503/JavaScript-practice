66e2072df149d67debbaa921419214bd61d29c68
onimuraaika <aika.onimura@gmail.com>
2021-03-09 23:00:17 +0900

    [Start/Add]クラスの定義練習

--- practice2.js
+++ practice2.js
@@ -1,8 +1,9 @@
-//戻り値のある関数の定義
-function cal(a, b, c) {
-  return a + b + c;
-};
+class Student {
 
-//変数に入れて結果を表示
-let result = cal(10, 5, 8);
-console.log(result);
+  avg() {
+    console.log((80 + 70) / 2);
+  }
+}
+
+let a001 = new Student();
+a001.avg();
