ed88800eb960beecc18eec3e8a6c6ee63833e3aa
onimuraaika <aika.onimura@gmail.com>
2021-03-09 22:39:54 +0900

    [Add]関数に引数を渡して呼び出す、計算させる(応用)

--- practice2.js
+++ practice2.js
@@ -1,8 +1,8 @@
 //戻り値のある関数の定義
-function cal(a,b) {
-  return a + b;
+function cal(a, b, c) {
+  return a + b + c;
 };
 
 //変数に入れて結果を表示
-let result = cal(1, 2);
+let result = cal(10, 5, 8);
 console.log(result);
