1cda7ca000fa48691b49161334e684824a3f7474
onimuraaika <aika.onimura@gmail.com>
2021-03-09 17:32:57 +0900

    [Start/Add]for文練習

--- practice2.js
+++ practice2.js
@@ -1,12 +1,3 @@
-let age = 5;
-
-if (age >= 10 && age < 20) {
-  console.log("10代");
-} else if (age >= 20 && age < 30) {
-  console.log("20代");
-} else if (age >= 30 && age < 40) {
-  console.log("30代");
-} else {
-  console.log("その他");
+for(let i = 0; i <= 5; i ++) {
+  console.log(i);
 }
-
