6d4be16184fc505dff2738dd5ef2cd66468a59d2
onimuraaika <aika.onimura@gmail.com>
2021-03-09 23:25:57 +0900

    [Add]クラスの定義練習(コンストラクタ)

--- practice2.js
+++ practice2.js
@@ -1,4 +1,7 @@
 class Student {
+  constructor() {
+    this.name = "";
+  }
 
   avg(math, english) {
     console.log((math + english) / 2);
@@ -7,6 +10,8 @@ class Student {
 
 let a001 = new Student();
 a001.name = "sato";
-a001.avg(80, 70);
-
 console.log(a001.name);
+
+let a002 = new Student();
+a002.name = "tanaka";
+console.log(a002.name);
